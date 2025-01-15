db54a573c3c65120f80b8546633ba88f8762796a
gabri3lxke5 <gabrielcarpenta3918dkd@wzofit.com>
2025-01-15 20:36:42 +0000

    Revert "Navbar component (#1)" (#2)
    This reverts commit 45dbbdea4990b187383adf97366ef5dccd2bc305.

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,7 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        primary: '#D74444',
-        'primary-hover': '#b63535',
-        'primary-focus': '#e05c5c',
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
