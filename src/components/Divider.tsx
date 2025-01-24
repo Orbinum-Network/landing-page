5fa818b3d23f3b2e185c9601ddcb4812ffbc2d91
gabri3lxke5 <gabrielcarpenta3918dkd@wzofit.com>
2025-01-24 10:45:40 -0300

    add reusable Divider component with gradient styling

---
+++ src/components/Divider.tsx
@@ -0,0 +1,7 @@
+export const Divider = () => {
+    return (
+        <span className="relative flex items-center">
+            <div className="flex-grow h-px bg-gradient-to-r from-transparent via-gray-500 to-transparent opacity-75"></div>
+        </span>
+    );
+};
