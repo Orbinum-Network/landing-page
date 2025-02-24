6974f745f726f78ba0a9f8ebb9eff9c6794455ac
gabri3lxke5 <gabrielcarpenta3918dkd@wzofit.com>
2025-02-24 15:19:30 -0300

    Add animated gradient divider component

---
+++ src/components/common/Divider.tsx
@@ -0,0 +1,8 @@
+export const Divider = () => {
+    return (
+        <span className="relative flex items-center justify-center w-full">
+            <div className="flex-grow h-px bg-gradient-to-r from-transparent via-gray-500 to-transparent opacity-75 animate-pulse">
+            </div>
+        </span>
+    );
+};
