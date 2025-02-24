a5f1413be7e7a7e9b8415a90b650d32287b25c54
gabri3lxke5 <gabrielcarpenta3918dkd@wzofit.com>
2025-02-24 15:20:23 -0300

    Export shared UI components for better modularity

---
+++ src/components/common/index.ts
@@ -0,0 +1,7 @@
+export * from "./Divider";
+export * from "./Navbar";
+export * from "./Footer";
+export * from "./Loader";
+export * from "./Promobar";
+export * from "./SectionTitle";
+export * from "./Modal";
