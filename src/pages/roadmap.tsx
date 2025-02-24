124cade31f2181153579ce8b8c8344189841020b
gabri3lxke5 <gabrielcarpenta3918dkd@wzofit.com>
2025-02-24 15:29:36 -0300

    Add RoadmapPage component with Roadmap integration

---
+++ src/pages/roadmap.tsx
@@ -0,0 +1,9 @@
+import { Roadmap } from "@/components/roadmap";
+
+export function RoadmapPage() {
+    return (
+        <main className="pt-16">
+            <Roadmap />
+        </main>
+    )
+}
