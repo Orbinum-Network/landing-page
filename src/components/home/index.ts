92c0ed4cc5f3a18fe12ea30586288f2a8c82ef9e
gabri3lxke5 <gabrielcarpenta3918dkd@wzofit.com>
2025-02-24 15:24:38 -0300

    Export all section components for improved modularity

---
+++ src/components/home/index.ts
@@ -0,0 +1,6 @@
+export * from "./HeroSection";
+export * from "./SdkSection";
+export * from "./FeaturesSection";
+export * from "./BlogSection";
+export * from "./AboutSection";
+export * from "./WhitepaperSection";
