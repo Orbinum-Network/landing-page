eae5a9b9135ae0b011654010a79ea232583b2bf6
gabri3lxke5 <gabrielcarpenta3918dkd@wzofit.com>
2025-02-24 14:59:32 -0300

    added router, routes and layout

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/index.css'
-import App from './App.tsx'
+import { BrowserRouter, Route, Routes } from "react-router";
+import { CareersPage, ContactPage, ErrorPage, HomePage, PrivacyPolicyPage, RoadmapPage, TermsPage, TokenomicsPage } from './pages';
+import { NavFooLayout } from './layout';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route element={<NavFooLayout />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/roadmap" element={<RoadmapPage />} />
+          <Route path="/tokenomics" element={<TokenomicsPage />} />
+          <Route path="/careers" element={<CareersPage />} />
+          <Route path="/careers/:id" element={<CareersPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/terms" element={<TermsPage />} />
+          <Route path="/privacy-policy" element={<PrivacyPolicyPage />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
